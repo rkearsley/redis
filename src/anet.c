ac4bc139928246f92c3dfd60b6848f9ea55a475a
Richard Kearsley <rkearsley@blueyonder.co.uk>
2013-04-12 17:14:12 +0100

    The sysctl(3) MIB variable kern.ipc.somaxconn specifies a hard limit on      backlog; if a value greater than kern.ipc.somaxconn or less than zero is      specified, backlog is silently forced to kern.ipc.somaxconn.

--- src/anet.c
+++ src/anet.c
@@ -323,7 +323,7 @@ static int anetListen(char *err, int s, struct sockaddr *sa, socklen_t len) {
     /* Use a backlog of 512 entries. We pass 511 to the listen() call because
      * the kernel does: backlogsize = roundup_pow_of_two(backlogsize + 1);
      * which will thus give us a backlog of 512 entries */
-    if (listen(s, 511) == -1) {
+    if (listen(s, -1) == -1) {
         anetSetError(err, "listen: %s", strerror(errno));
         close(s);
         return ANET_ERR;
